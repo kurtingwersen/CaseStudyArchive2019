4d4744e6c5af5f9195ddf03c857889a84aab71b1
Kurt Ingwersen <kurtingwersen@gmail.com>
2019-09-07 12:39:28 +1000

    Merge
    Merge from first repo

---
+++ .cache/async-requires.js
@@ -0,0 +1,22 @@
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+exports.components = {
+  "component---cache-dev-404-page-js": require("gatsby-module-loader?name=component---cache-dev-404-page-js!/Users/kurtingwersen/Documents/CaseStudyArchive2019/CaseStudyArchive2019/.cache/dev-404-page.js"),
+  "component---src-pages-404-js": require("gatsby-module-loader?name=component---src-pages-404-js!/Users/kurtingwersen/Documents/CaseStudyArchive2019/CaseStudyArchive2019/src/pages/404.js"),
+  "component---src-pages-index-js": require("gatsby-module-loader?name=component---src-pages-index-js!/Users/kurtingwersen/Documents/CaseStudyArchive2019/CaseStudyArchive2019/src/pages/index.js"),
+  "component---src-pages-page-2-js": require("gatsby-module-loader?name=component---src-pages-page-2-js!/Users/kurtingwersen/Documents/CaseStudyArchive2019/CaseStudyArchive2019/src/pages/page-2.js")
+}
+
+exports.json = {
+  "layout-index.json": require("gatsby-module-loader?name=path---!/Users/kurtingwersen/Documents/CaseStudyArchive2019/CaseStudyArchive2019/.cache/json/layout-index.json"),
+  "dev-404-page.json": require("gatsby-module-loader?name=path---dev-404-page!/Users/kurtingwersen/Documents/CaseStudyArchive2019/CaseStudyArchive2019/.cache/json/dev-404-page.json"),
+  "404.json": require("gatsby-module-loader?name=path---404!/Users/kurtingwersen/Documents/CaseStudyArchive2019/CaseStudyArchive2019/.cache/json/404.json"),
+  "index.json": require("gatsby-module-loader?name=path---index!/Users/kurtingwersen/Documents/CaseStudyArchive2019/CaseStudyArchive2019/.cache/json/index.json"),
+  "page-2.json": require("gatsby-module-loader?name=path---page-2!/Users/kurtingwersen/Documents/CaseStudyArchive2019/CaseStudyArchive2019/.cache/json/page-2.json"),
+  "404-html.json": require("gatsby-module-loader?name=path---404-html!/Users/kurtingwersen/Documents/CaseStudyArchive2019/CaseStudyArchive2019/.cache/json/404-html.json")
+}
+
+exports.layouts = {
+  "layout---index": require("gatsby-module-loader?name=component---src-layouts-index-js!/Users/kurtingwersen/Documents/CaseStudyArchive2019/CaseStudyArchive2019/.cache/layouts/index.js")
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,16 @@ import React from 'react'
 import Link from 'gatsby-link'
 
 const IndexPage = () => (
-  <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+<div>
+  <div className="Hero">
+    <div className="HeroGroup">
+      <h1>Here's a list of <br />Case Studies</h1>
+      <p>Please use these case studies as inspiration</p>
+      <p>Now go build something great.</p>
+      <Link to="/page-2/">Watch the video</Link>
+    </div>
   </div>
+</div>
 )
 
 export default IndexPage
