fef24b40750e6d40959af8127f762ecfec9cf451
Kurt Ingwersen <kurtingwersen@gmail.com>
2019-09-07 14:01:30 +1000

    Brenton Test

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,9 +5,8 @@ const IndexPage = () => (
 <div>
   <div className="Hero">
     <div className="HeroGroup">
-      <h1>Here's a list of <br />Case Studies</h1>
+      <h1>G'day Brenton</h1>
       <p>Please use these case studies as inspiration</p>
-      <p>Now go build something great.</p>
       <Link to="/page-2/">Watch the video</Link>
     </div>
   </div>
